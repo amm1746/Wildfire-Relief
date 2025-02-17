8ef181a61eb9337651863e93478cae3907289ce3
Alexandra Mantagas <amm1746@rit.edu>
2025-02-17 17:57:49 -0500

    Added CupboardFileDAO skeleton and implemented create need

--- ufund-api/src/main/java/com/ufund/api/ufundapi/dao/CupboardFileDAO.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/dao/CupboardFileDAO.java
@@ -6,4 +6,35 @@
 
  package com.ufund.api.ufundapi.dao;
 
- 
+ import com.ufund.api.ufundapi.model.Need;
+ import java.util.*;
+
+ public class CupboardFileDAO implements CupboardDAO{
+    List<Need> needs = new ArrayList<>();
+    @Override
+    public Need createNeed(Need need){
+        if(needExists(need.getName())){
+            return null;
+        }
+        else{
+            needs.add(need);
+            return need; 
+        }
+    }
+    @Override 
+    public Need needByName(String name){
+        return null;
+    }
+    @Override
+    public List<Need> getAllNeeds(){
+        return needs;
+    }
+    @Override
+    public void deleteNeed(String name){
+
+    }
+    @Override
+    public boolean needExists(String name){
+        return false;
+    }
+ }
