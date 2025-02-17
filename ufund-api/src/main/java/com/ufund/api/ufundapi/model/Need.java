c22f5d91d1c38a32d4a880da90fdf271b874a37e
Alexandra Mantagas <amm1746@rit.edu>
2025-02-17 15:40:09 -0500

    Finished Need class implementation

--- ufund-api/src/main/java/com/ufund/api/ufundapi/model/Need.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/model/Need.java
@@ -7,8 +7,17 @@
  package com.ufund.model; 
 
  public class Need {
+    private String name;
+    private double cost;
+    private int quantity;
+    private String type; 
 
+    public Need(String name, double cost, int quantity, String type){
+        this.name = name;
+        this.cost = cost;
+        this.quantity = quantity;
+        this.type = type;
+    }
  }
 
  //getters and setters 
- 
