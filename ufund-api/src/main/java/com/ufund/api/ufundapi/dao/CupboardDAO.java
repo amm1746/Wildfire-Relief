04a5b3ca9c57e223a3084eef3feea6c539837094
sophi <sel5881@g.rit.edu>
2025-02-17 17:10:59 -0500

    Fixed the missing package error in CupboardDAO and CupboardFileDAO files

--- ufund-api/src/main/java/com/ufund/api/ufundapi/dao/CupboardDAO.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/dao/CupboardDAO.java
@@ -6,8 +6,9 @@
 
  package com.ufund.api.ufundapi.dao;
 
- import com.ufund.model.Need;
  import java.util.List;
+
+ import com.ufund.api.ufundapi.model.Need;
 
  public interface CupboardDAO{
     Need needByName(String name);
