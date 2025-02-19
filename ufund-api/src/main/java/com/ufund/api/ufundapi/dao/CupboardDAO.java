b7b4ef4b33bc6c3451678506a92a80b481d343ef
Alexandra Mantagas (RIT Student) <amm1746@rit.edu>
2025-02-19 14:17:37 -0500

    Added documentation to CupboardDAO.java

--- ufund-api/src/main/java/com/ufund/api/ufundapi/dao/CupboardDAO.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/dao/CupboardDAO.java
@@ -1,22 +1,76 @@
+package com.ufund.api.ufundapi.dao;
+import java.io.IOException;
+import java.util.List;
+import com.ufund.api.ufundapi.model.Need;
+
 /**
- * Interface for cupboard.
+ * Interface for managing needs in the cupboard.
  *
  * @author Alexandra Mantagas
  */
 
- package com.ufund.api.ufundapi.dao;
- import java.io.IOException;
- import java.util.List;
-
- import com.ufund.api.ufundapi.model.Need;
-
  public interface CupboardDAO{
+   /**
+    * Gets a list of needs that match given name.
+    *
+    * @param name The name of need (partial or full).
+    * @return A list of needs that match.
+    */
+
     List<Need> needByName(String name);
+
+    /**
+    * Gets a need by name.
+    *
+    * @param name The name of need.
+    * @return The matching need, otherwise null.
+    */
+
     Need getNeed(String name);
+
+   /**
+    * Creates a new need.
+    *
+    * @param name The name of need being created.
+    * @return The created need.
+    * @throws IOException If an error occurs.
+    */
+
     Need createNeed(Need need) throws IOException;
+
+   /**
+    * Gets all needs in cupboard.
+    *
+    * @return A list of needs. 
+    */
+
     List<Need> getAllNeeds();
+
+   /**
+    * Deletes a need by name. 
+    *
+    * @param name The name of need being deleted.
+    */
+
     void deleteNeed(String name);
+
+   /**
+    * Checks if a need exists or not.
+    *
+    * @param name The name of need.
+    * @return True if the need exists, otherwise false. 
+    */
+
     boolean needExists(String name);
+
+   /**
+    * Updates an existing need.
+    *
+    * @param name The name of need being updated.
+    * @param updated The updated need.
+    * @return The updated need.
+    */
+    
     Need updateNeed(String name, Need updated);
  }
 
