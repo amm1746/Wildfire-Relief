a23edb31cd21964258f9e5fab56d1383ef8aed0a
evanlin3 <el9716@rit.edu>
2025-02-24 18:13:03 -0500

    Start of Manager file.

--- ufund-api/src/main/java/com/ufund/api/ufundapi/log/Manager.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/log/Manager.java
@@ -1,5 +1,16 @@
 package com.ufund.api.ufundapi.log;
+import java.io.IOException;
+import java.util.List;
+import com.ufund.api.ufundapi.model.Need;
 
-public class Manager {
-    
+/**
+ * Interface for the manager.
+ *
+ * @author Evan Lin
+ */
+
+public interface Manager 
+{
+    Need create(String name, double cost, int quantity, String type);
+
 }
