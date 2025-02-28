bf2a7f1bfa4b8b77cafa524a4af42a286ec5f81c
sophi <sel5881@g.rit.edu>
2025-02-28 16:06:11 -0500

    Implemented deleteNeed() and updateNeed() functions for the U-fund Manager. Also added documentation for the Manager class

--- ufund-api/src/main/java/com/ufund/api/ufundapi/log/Manager.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/log/Manager.java
@@ -6,24 +6,60 @@ import com.ufund.api.ufundapi.dao.CupboardDAO;
 import com.ufund.api.ufundapi.model.Need;
 
 /**
- * Interface for the manager.
+ * Manager class for handling operations related to needs
+ * in the cupboard. Implements logic for modifying stored
+ * needs.
  *
  * @author Evan Lin
+ * @author Sophia Le
  */
 
 public class Manager {
-    public static CupboardDAO cupboardDAO = null;
+    // initializes an empty cupboard
+    private static CupboardDAO cupboardDAO = null;
     
     public Manager(CupboardDAO cupboardDAO) {
             this.cupboardDAO = cupboardDAO;
     }
 
+    /**
+     * Retrieves all stored needs in the cupboard
+     * @return a list of all needs
+     */
     public List<Need> viewAllNeeds() {
         return cupboardDAO.getAllNeeds();
     }
 
+    /**
+     * Adds a new need to the cupboard.
+     * @param need the need object to be added
+     * @return the created need if successful, otherwise null if the need already exists
+     * @throws IOException if an error occurs while saving
+     */
     public Need addNeed(Need need) throws IOException {
         return cupboardDAO.createNeed(need);
+    }
+
+    /**
+     * Updates an existing need in the cupboard
+     * @param name the name of the need to be updated
+     * @param updated the updated need object
+     * @return the updated need if successful, otherwise null if not found
+     */
+    public Need updateNeed(String name, Need updated) {
+        return cupboardDAO.updateNeed(name, updated);
+    }
+    /**
+     * Deletes a need from the cupboard by its name
+     * @param name the name of the need to be deleted
+     * @return True if the deletion was successful, otherwise False
+     */
+    public boolean deleteNeed(String name) {
+        if(cupboardDAO.needExists(name)) {
+            cupboardDAO.deleteNeed(name);
+            return true;
+        }
+        return false;
     }
     // Need create(String name, double cost, int quantity, String type);
 
