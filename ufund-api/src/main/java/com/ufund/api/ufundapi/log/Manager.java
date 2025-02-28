f5a2459d8605d28400692d80c7399f503039529d
sophi <sel5881@g.rit.edu>
2025-02-28 16:00:24 -0500

    Implemented U-fund Manager constructor. Also implemented viewAllNeeds() and addNeed() functions for the Manager

--- ufund-api/src/main/java/com/ufund/api/ufundapi/log/Manager.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/log/Manager.java
@@ -1,6 +1,8 @@
 package com.ufund.api.ufundapi.log;
 import java.io.IOException;
 import java.util.List;
+
+import com.ufund.api.ufundapi.dao.CupboardDAO;
 import com.ufund.api.ufundapi.model.Need;
 
 /**
@@ -9,8 +11,20 @@ import com.ufund.api.ufundapi.model.Need;
  * @author Evan Lin
  */
 
-public interface Manager 
-{
-    Need create(String name, double cost, int quantity, String type);
+public class Manager {
+    public static CupboardDAO cupboardDAO = null;
+    
+    public Manager(CupboardDAO cupboardDAO) {
+            this.cupboardDAO = cupboardDAO;
+    }
+
+    public List<Need> viewAllNeeds() {
+        return cupboardDAO.getAllNeeds();
+    }
+
+    public Need addNeed(Need need) throws IOException {
+        return cupboardDAO.createNeed(need);
+    }
+    // Need create(String name, double cost, int quantity, String type);
 
 }
