b9ddcb70729dc234cdb8a3d071adc41d953e91fb
amm1746 <amm1746@rit.edu>
2025-03-02 16:29:44 -0500

    Created the Login class

---
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/controller/LoginController.java
@@ -0,0 +1,12 @@
+package com.ufund.controller;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "*")
+
+public class Login{
+
+}
