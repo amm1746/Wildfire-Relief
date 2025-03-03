fab8dd8a1757a66cf7a977c60ac8be2d1d0381ff
amm1746 <amm1746@rit.edu>
2025-03-02 20:44:18 -0500

    Connected the input fields to username and password, called AuthService for login

--- ufund-ui/wildfire-relief/src/app/components/login/login.component.ts
+++ ufund-ui/wildfire-relief/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
+  role: string | null = null;
+  error: string | null = null; 
 
+  constructor(private authService: AuthService) {}
+
+  login(): void{
+    this.authService.login(this.username, this.password).subscribe(
+      response => {
+        this.role = response.role;
+        this.error = null;
+      },
+      error => {
+        this.error = 'Incorrect username or password.';
+        this.role = null;
+      }
+    );
+  }
 }
