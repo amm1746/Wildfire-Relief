7d4ed2fceb18c0303297bf6190a1ef3e8bebeffd
amm1746 <amm1746@rit.edu>
2025-03-03 10:30:27 -0500

    Added a 'before each' test to setup LoginController and a mock Http session before each test

--- ufund-api/src/test/java/com/ufund/api/ufundapi/UfundApiApplicationTests.java
+++ ufund-api/src/test/java/com/ufund/api/ufundapi/UfundApiApplicationTests.java
@@ -1,15 +1,35 @@
 package com.ufund.api.ufundapi;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpSession;
+
+import com.ufund.api.ufundapi.controller.LoginController;
+
+import jakarta.servlet.http.HttpSession;
 
 @SpringBootTest
 class UfundApiApplicationTests {
 
+	private LoginController loginController;
+	private HttpSession session;
+	
+	@BeforeEach
+	void start(){
+		loginController = new LoginController();
+		session = new MockHttpSession();
+	}
+
 	@Test
 	void contextLoads() {
 		assertTrue(true);
+	}
+
+	@Test
+	void testManagerLogin(){
+		
 	}
 
 }
