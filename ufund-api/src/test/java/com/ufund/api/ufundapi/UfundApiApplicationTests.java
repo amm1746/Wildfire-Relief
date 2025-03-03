f960bf01b63cd69a3624aa3cefb6f160eea18c4f
amm1746 <amm1746@rit.edu>
2025-03-03 10:25:15 -0500

    Added assertion statement to check if app starts

--- ufund-api/src/test/java/com/ufund/api/ufundapi/UfundApiApplicationTests.java
+++ ufund-api/src/test/java/com/ufund/api/ufundapi/UfundApiApplicationTests.java
@@ -1,5 +1,6 @@
 package com.ufund.api.ufundapi;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class UfundApiApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue(true);
 	}
 
 }
