ed15d1d79355e7e5aea915c153c46e27d7c8463d
sophi <sel5881@g.rit.edu>
2025-03-03 16:04:38 -0500

    Created a Helper test file. Implemented unit tests for each of the Helper class methods

---
+++ ufund-api/src/test/java/com/ufund/api/ufundapi/HelperTest.java
@@ -0,0 +1,64 @@
+package com.ufund.api.ufundapi;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.ufund.api.ufundapi.log.Helper;
+import com.ufund.api.ufundapi.model.Need;
+
+import java.util.List;
+
+public class HelperTest {
+
+    private Helper helper;
+
+    @BeforeEach
+    public void setup() {
+        helper = new Helper();
+    }
+
+    @Test
+    public void testAddToFundingBasket() {
+        Need need = new Need("Need1", 10.0, 1, "Type1");
+        assertTrue(helper.addToFundingBasket(need));
+        assertEquals(1, helper.getFundingBasket().size());
+        assertEquals("Need1", helper.getFundingBasket().get(0).getName());
+    }
+
+    @Test
+    public void testAddDuplicateNeed() {
+        Need need = new Need("Need1", 10.0, 1, "Type1");
+        helper.addToFundingBasket(need);
+        assertFalse(helper.addToFundingBasket(need)); // Should not allow duplicate
+        assertEquals(1, helper.getFundingBasket().size());
+    }
+
+    @Test
+    public void testRemoveFromFundingBasket() {
+        Need need = new Need("Need1", 10.0, 1, "Type1");
+        helper.addToFundingBasket(need);
+        assertTrue(helper.removeFromFundingBasket(need));
+        assertEquals(0, helper.getFundingBasket().size());
+    }
+
+    @Test
+    public void testRemoveNonExistentNeed() {
+        Need need = new Need("Need1", 10.0, 1, "Type1");
+        assertFalse(helper.removeFromFundingBasket(need)); // Should return false
+    }
+
+    @Test
+    public void testGetFundingBasket() {
+        Need need1 = new Need("Need1", 10.0, 1, "Type1");
+        Need need2 = new Need("Need2", 20.0, 2, "Type2");
+        helper.addToFundingBasket(need1);
+        helper.addToFundingBasket(need2);
+
+        List<Need> basket = helper.getFundingBasket();
+        assertEquals(2, basket.size());
+        assertTrue(basket.contains(need1));
+        assertTrue(basket.contains(need2));
+    }
+}
